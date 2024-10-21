9a28434bca414070a849f5329b37c1f2836c38cc
molnar-david <d.molnarka@hotmail.com>
2024-10-21 20:36:36 +0200

    Implement insert and deleteItem methods

--- Tree.js
+++ Tree.js
@@ -19,7 +19,6 @@ export default class Tree {
         if (!this.#root.value) {
             array = this.sortAndRemoveDuplicates(array);
             currentNode = this.#root;
-            console.log(array);
         }
         const midPoint = Math.floor((array.length - 1) / 2);
         currentNode.value = array[midPoint];
@@ -32,14 +31,78 @@ export default class Tree {
 
     prettyPrint(node, prefix = "", isLeft = true) {
         if (node.value === null) {
-          return;
+            return;
         }
         if (node.rightNode !== null) {
-          this.prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+            this.prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
         }
         console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
         if (node.leftNode !== null) {
-          this.prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+            this.prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
         }
-      }
+    }
+
+    insert(value) {
+        let currentNode = this.#root;
+        let prevNode = null;
+        while (currentNode) {
+            prevNode = currentNode;
+            if (value < currentNode.value) {
+                currentNode = currentNode.leftNode;
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.rightNode;
+            } else return;
+        }
+        if (prevNode) {
+            const newNode = new Node(value);
+            value < prevNode.value ? prevNode.leftNode = newNode : prevNode.rightNode = newNode;
+        }
+    }
+
+    deleteItem(value) {
+        let currentNode = this.#root;
+        let targetNode = null;
+        let parentNode = null;
+        while (currentNode) {
+            if (value === currentNode.value) {
+                targetNode = currentNode;
+                break;
+            }
+            parentNode = currentNode;
+            value < currentNode.value ? currentNode = currentNode.leftNode : currentNode = currentNode.rightNode;
+        }
+        if (targetNode) {
+            if (!targetNode.leftNode && !targetNode.rightNode) {
+                if (targetNode === this.#root) {
+                    this.#root = new Node();
+                } else {
+                    targetNode.value > parentNode.value ? parentNode.rightNode = null : parentNode.leftNode = null;
+                }
+            } else if (!targetNode.leftNode) {
+                if (targetNode === this.#root) {
+                    this.#root = targetNode.rightNode;
+                } else {
+                    targetNode.value > parentNode.value ? parentNode.rightNode = targetNode.rightNode : parentNode.leftNode = targetNode.rightNode;
+                }
+            } else if (!targetNode.rightNode) {
+                if (targetNode === this.#root) {
+                    this.#root = targetNode.leftNode;
+                } else {
+                    targetNode.value > parentNode.value ? parentNode.rightNode = targetNode.leftNode : parentNode.leftNode = targetNode.leftNode;
+                }
+            } else {
+                // parentNode is not used here, so no need to check for #root
+                // Search for inorder successor
+                let successorValue = null;
+                currentNode = currentNode.rightNode;
+                while (currentNode) {
+                    successorValue = currentNode.value;
+                    currentNode = currentNode.leftNode;
+                }
+                // Delete node with a single child
+                this.deleteItem(successorValue);
+                targetNode.value = successorValue;
+            }
+        }
+    }
 }
--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const tree = new Tree();
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 tree.buildTree(array);
 tree.prettyPrint(tree.root);
+tree.insert(11);
+tree.prettyPrint(tree.root);
+tree.deleteItem(3);
+tree.prettyPrint(tree.root);
